421e25d146b98c7ae60056cd18e98ddfddfff7c6
Othávio_Nakamura <103123718+othavionakamura@users.noreply.github.com>
2022-09-12 09:31:52 -0300

    atualização

--- cad_cont_list.php
+++ cad_cont_list.php
@@ -1,17 +1,51 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
+        .card{
+            text-align: center;
+        }
+
         .img{
-            height: 100px;
+            height: 150px;
+            width: 150px;
+            background-color: white;
+            border-radius: 50%;
+            object-fit: cover;
+            object-position: center;
+            position: relative;
+            z-index: 1;
+        }
+        
+        .conteudo{
+            background-color: orange;
+            text-align: center;
+            height: 300px;
+            width: 350px;
+            position: relative;
+            top: -110px;
+            z-index: 0;
+            border-radius: 30px;
         }
     </style>
 </head>
 <body>
+
+    <div class="card">
+        <img src='images/2022.09.09-13.57.48.png' class='img'>
+        <div class="conteudo">
+            <h2>João</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Phasellus aliquet lectus ac tristique posuere. Praesent a 
+            tellus elementum, interdum libero in, tristique lectus. 
+            Maecenas efficitur, urna sit amet laoreet interdum.</p>
+        </div>
+    </div>
+
     <?php
         require ('conexão.php');
         try {
